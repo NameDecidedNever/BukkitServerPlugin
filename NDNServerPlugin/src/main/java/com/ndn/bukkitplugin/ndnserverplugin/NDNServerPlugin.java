a33303343e1d99d342fa14e18aceb1042699a104
Matthew Smith <matthew.mcs.smith@gmail.com>
2019-03-29 18:48:11 -0700

    First Commit
    Basic setup of file system and Main class

---
+++ NDNServerPlugin/src/main/java/com/ndn/bukkitplugin/ndnserverplugin/NDNServerPlugin.java
@@ -0,0 +1,22 @@
+package com.ndn.bukkitplugin.ndnserverplugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NDNServerPlugin extends JavaPlugin{
+    @Override
+    public void onEnable() {
+        // TODO Insert logic to be performed when the plugin is enabled
+    }
+    
+    @Override
+    public void onDisable() {
+        // TODO Insert logic to be performed when the plugin is disabled
+    }
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    	// TODO Handle commands for plugin
+    	return true;
+    }
+}
