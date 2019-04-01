72765f7538257d6850f283d7d493df13ac92735f
Matthew Smith <matthew.mcs.smith@gmail.com>
2019-03-31 19:26:20 -0700

    Player payment support
    Added commands that allow players to transfer and check their balances.

--- NDNServerPlugin/src/main/java/com/ndn/bukkitplugin/ndnserverplugin/NDNServerPlugin.java
+++ NDNServerPlugin/src/main/java/com/ndn/bukkitplugin/ndnserverplugin/NDNServerPlugin.java
@@ -1,6 +1,7 @@
 package com.ndn.bukkitplugin.ndnserverplugin;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -25,10 +26,27 @@ public class NDNServerPlugin extends JavaPlugin implements Listener{
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
     	if (sender instanceof Player) {
+    		DataManager dataManger = DataManager.getInstance();
             Player player = (Player) sender;
             switch (cmd.getName()) {
             case "account":
-            	
+            	if(args[0].equals("bal") || args[0].equals("balance")) {
+            		String balancePrompt = ChatColor.GREEN + "Account Balance : ";
+            		String balance = ChatColor.YELLOW + "$" + dataManger.getPlayerBalance(player.getName());
+            		player.sendMessage(balancePrompt + balance);
+            	}else if(args[0].equals("send")) {
+            		if(getServer().getPlayer(args[1]) != null) {
+            		if(dataManger.makePayExchange(dataManger.getPlayerPrimaryAccount(player.getName()), dataManger.getPlayerPrimaryAccount(args[1]), Double.parseDouble(args[2]), args.length > 3 ? args[3] : "Player to Player Transaction")) {            			
+            			player.sendMessage(ChatColor.GREEN + "Transaction Complete!");
+            			Bukkit.getPlayer(args[1]).sendMessage(ChatColor.GREEN + "You've just recieved " + ChatColor.YELLOW + "$" + args[2] + ChatColor.GREEN + " from " + ChatColor.BLUE + player.getName());
+            		} else {
+            			player.sendMessage(ChatColor.RED + "Insufficient Funds");
+            		}
+            		}else {
+            			player.sendMessage(ChatColor.RED + "That player doesn't exist...");
+            		}
+            	}
+            	return true;
             case "city":
             	
             case "stock":
@@ -45,7 +63,10 @@ public class NDNServerPlugin extends JavaPlugin implements Listener{
     	DataManager.getInstance().addPlayerIfNotExists(evt.getPlayer().getName());
     	if(!DataManager.getInstance().checkPlayerIsVerified(evt.getPlayer().getName())) {
     		//Player is not verified, we need to give them a verification code.
-    		evt.getPlayer().sendMessage("Your Website Verification Code : " + DataManager.getInstance().getPlayerVerificationCode(evt.getPlayer().getName()));
+    		String prompt = ChatColor.GREEN + "Your Website Verification Code : ";
+    		String code = ChatColor.YELLOW + "" + DataManager.getInstance().getPlayerVerificationCode(evt.getPlayer().getName());
+    		evt.getPlayer().sendMessage(prompt + code);
     	}
     }
 }
+  
