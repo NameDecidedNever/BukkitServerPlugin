b84c125cef45eac446528f7dea761b1c5105eb24
chmilk1(Home) <jamesmaron87@gmail.com>
2019-04-16 20:39:50 -0700

    I JUST DID

--- NDNServerPlugin/src/main/java/com/ndn/bukkitplugin/ndnserverplugin/NDNServerPlugin.java
+++ NDNServerPlugin/src/main/java/com/ndn/bukkitplugin/ndnserverplugin/NDNServerPlugin.java
@@ -23,8 +23,6 @@ public class NDNServerPlugin extends JavaPlugin implements Listener {
 	public void onEnable() {
 		// TODO Insert logic to be performed when the plugin is enabled
 		Bukkit.getServer().getPluginManager().registerEvents(this, this);
-		Bukkit.getServer().getPluginManager().registerEvents(new MobMoney(), this);
-		Bukkit.getServer().getPluginManager().registerEvents(new AchievementRewards(), this);
 		mce = new MoneyCommandExecutor(this);
 		spce = new SimplePaidCommandExecutor(this);
 		
